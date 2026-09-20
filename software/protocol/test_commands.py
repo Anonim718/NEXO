d20e8308b9a176ad60ed15fc4812fa238f8cbcde
SILVER <130654358+Anonim718@users.noreply.github.com>
2026-09-20 23:52:11 +0200

    Add protocol tests

---
+++ software/protocol/test_commands.py
@@ -0,0 +1,10 @@
+from commands import Command, format_command, parse_command
+
+def test_parse_normalizes_input():
+    assert parse_command("  forward ") is Command.FORWARD
+
+def test_unknown_command_is_rejected():
+    assert parse_command("DELETE_ALL") is None
+
+def test_format_adds_protocol_terminator():
+    assert format_command(Command.STOP) == "STOP\n"
