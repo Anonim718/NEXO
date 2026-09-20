212945590ee5df19121891dd86e59894ee26e40c
SILVER <130654358+Anonim718@users.noreply.github.com>
2026-09-21 00:00:08 +0200

    Fix Python test import path

--- software/test_behaviour.py
+++ software/test_behaviour.py
@@ -1,4 +1,4 @@
-from behaviour import BehaviourPolicy
+from software.behaviour import BehaviourPolicy
 
 def test_safety_response_is_deterministic():
     assert "safety" in BehaviourPolicy().response_for("STOP","obstacle_detected").lower()
